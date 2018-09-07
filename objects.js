9fb51a8003e47fe113155d179f6782a6cff91ddb
Jon Mackey <jon@jonmackey.party>
2018-09-07 19:58:48 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { ['Slowdive']: 'Alison', ['My Bloody Valentine']: 'Sometimes'};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  var artistName = 'Phil Ochs';
+  var songTitle = 'Here/s to the State of Mississippi';
+  return Object.assign ({}, playlist, {artistName: songTitle});
+}
+
+function removeFromPlaylist(playlist, artistName){
+  return 
+}
